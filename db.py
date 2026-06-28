941dbe90075e25d6da3dc21e513f7f0c9e849853
shaik_raza <ugs24196_it.razamohammad@cbit.org.in>
2026-06-28 12:59:15 +0530

    Fix: hide credentials using .env

--- db.py
+++ db.py
@@ -1,11 +1,14 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
 
-# Connect to MySQL
+load_dotenv()
+
 db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="raza.1290",
-    database="student_tracker"
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
 )
 
 cursor = db.cursor()
